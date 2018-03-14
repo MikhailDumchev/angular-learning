8bff2740bcfa3b45096fd2bdb0a750fef12cf7bf
MikhailDumchev <mikhail_dumchev@ukr.net>
2018-03-14 23:58:02 +0200

    Пройден второй урок

--- js/app.js
+++ js/app.js
@@ -2,7 +2,19 @@
 (function() {
     var store = angular.module("store", []);
     store.controller("DisplayController", function() {
-        console.log(model);
         this.products = model.items;
+    });
+    store.controller("PanelController", function() {
+        var currentTab = 1;
+        this.CurrentTab = function(value) {
+            if (!arguments.length) return currentTab;
+            else currentTab = value;
+        };
+        this.check = function(value) {
+            return currentTab === value;
+        };
+    });
+    store.controller("ChatController", function() {
+        this.opinions = opinions.items;
     });
 }());
--- js/model.js
+++ js/model.js
@@ -1,8 +1,26 @@
 var model = {
     items: [
-        { purchase: "Хлеб", presence: false, price: 15.9 },
+        { purchase: "Хлеб",
+            presence: true, 
+            price: 15.9,
+            src: "img/bread.jpg"
+        },
         { purchase: "Масло", presence: false, price: 60 },
-        { purchase: "Картофель", presence: true, price: 22.6 },
+        { purchase: "Картофель", presence: true, price: 22.6, src: "img/potato.png" },
         { purchase: "Сыр", presence: false, price: 310 }
+    ]
+};
+var opinions = {
+    items: [
+        {
+            stars: 4,
+            message: "Неплохой продукт",
+            author: "Misha"
+        },
+        {
+            stars: 1,
+            message: "Не понравилось",
+            author: "Лена"
+        }
     ]
 };
